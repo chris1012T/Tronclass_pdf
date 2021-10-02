5f1293ffc8db2539c06ffe107d756272b86b2ffb
james20001012 <dhps40303@gmail.com>
2021-10-02 23:58:47 +0800

    v1.1 initiate

--- fetch.js
+++ fetch.js
@@ -3,4 +3,6 @@ if (document.getElementById("pdf-viewer") != null) {
     var srcarr = src.split("?file=");
     src = decodeURIComponent(srcarr[1]);
     chrome.runtime.sendMessage({ src: src });
+} else if (document.querySelector(".course-activity-tabs")) {
+    alert("請先開啟教材!");
 }
