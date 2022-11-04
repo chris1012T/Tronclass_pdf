f030949aa56f83d3f7a2bc2f3d56a8417bb4a5d3
james20001012 <dhps40303@gmail.com>
2022-11-05 01:06:21 +0800

    v1.3.2
    docs: Changed the background color and the caution class color in the doc
    
    refactor: Optimized the code in fetch.js

--- fetch.js
+++ fetch.js
@@ -1,7 +1,5 @@
 if (document.getElementById("pdf-viewer") != null) {
-    var src = document.getElementById("pdf-viewer").src;
-    var srcarr = src.split("?file=");
-    src = decodeURIComponent(srcarr[1]);
+    let src = decodeURIComponent(document.getElementById("pdf-viewer").src.split("?file=")[1]);
     chrome.runtime.sendMessage({ src: src });
 } else if (document.querySelector(".course-header-container") || document.querySelector(".activity-title-wrapper")) {
     alert("請先開啟教材!");
